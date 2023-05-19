db45213f7cb1d2841d6b6fe2ceef5a6c708e5647
rajibrahman74e <rajiburrahman74@gmail.com>
2023-05-20 01:30:55 +0600

    [ADD]-single-hero-data-get-operations

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const cors = require('cors');
 require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express()
 const port = process.env.PORT || 5000
 
@@ -49,6 +49,17 @@ async function run() {
             const result = await superHerosCollection.insertOne(toys)
             res.send(result)
         })
+
+
+        // get operations
+        app.get("/herotoys/:id", async(req,res)=>{
+            const id = req.params.id;
+            const query = {_id: new ObjectId(id)}
+            const result = await superHerosCollection.findOne(query)
+            res.send(result);
+        })
+
+        // 
 
 
 
